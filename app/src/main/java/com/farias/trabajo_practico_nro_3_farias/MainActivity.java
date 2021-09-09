4be7b65b796d1d688def77a407a28a17cf15e35f
gfsanluis78 <71910385+gfsanluis78@users.noreply.github.com>
2021-09-09 09:57:22 -0300

    Initial commit

---
+++ app/src/main/java/com/farias/trabajo_practico_nro_3_farias/MainActivity.java
@@ -0,0 +1,13 @@
+package com.farias.trabajo_practico_nro_3_farias;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
